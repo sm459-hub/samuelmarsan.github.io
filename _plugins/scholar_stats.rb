7d2a02d8d5ccd1d6c62b6e2d99c7dc312ee6b6d8
sm459-hub <samuel.marsan@duke.edu>
2025-08-03 16:33:43 -0400

    Create scholar_stats.rb

---
+++ _plugins/scholar_stats.rb
@@ -0,0 +1,35 @@
+require 'open-uri'
+require 'nokogiri'
+
+module Jekyll
+  class ScholarProfileTag < Liquid::Tag
+    def render(context)
+      user_id = context.registers[:site].config['google_scholar_id']
+      profile_url = "https://scholar.google.com/citations?user=#{user_id}&hl=en"
+      begin
+        html = URI.open(profile_url).read
+        doc = Nokogiri::HTML.parse(html)
+        publications = doc.css('.gsc_a_tr').first(5) # Top 5 only
+
+        output = "<ul class='scholar-publications'>"
+        publications.each do |pub|
+          title = pub.at_css('.gsc_a_at')&.text
+          link = pub.at_css('.gsc_a_at')&.[]('href')
+          authors = pub.at_css('.gs_gray')&.text
+          journal = pub.css('.gs_gray')[1]&.text
+
+          output += "<li>"
+          output += "<strong><a href='https://scholar.google.com#{link}' target='_blank' rel='noopener'>#{title}</a></strong><br>"
+          output += "#{authors}<br><em>#{journal}</em>"
+          output += "</li>"
+        end
+        output += "</ul>"
+        output
+      rescue => e
+        "<p>Unable to load Google Scholar feed: #{e.message}</p>"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('scholar_profile', Jekyll::ScholarProfileTag)
